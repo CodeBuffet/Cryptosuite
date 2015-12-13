1f57a6e6d8dd41474df647ba92ba4517e4e7db1b
Bud Nail <budnail@gmail.com>
2015-12-13 16:40:10 -0600

    Create Arduino.h
     Created for compatibility with Particle.o's Photon

---
+++ Sha/Arduino.h
@@ -0,0 +1,5 @@
+/ This is a special Arduino.h for Particle.io's Photon  device
+#include <Particle.h> //use for compatibility with standard Arduino libraries
+#include <stdint.h>
+#define __PHOTON__ //used to exclude some includes
+
